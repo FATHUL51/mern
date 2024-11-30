2bde8a453c130f8e3a604c491dfbe083e5cfda5e
Sk Fathul Islam <178652788+FATHUL51@users.noreply.github.com>
2024-11-30 14:49:42 +0530

    second commit

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const path = require("path");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const userRoute = require("./routes/user");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("db connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
-app.listen(3000, () => {
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api/user", userRoute);
+app.listen(PORT, () => {
   console.log("server started on port 3000");
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("db connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
---
+++ routes/user.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../schema/user.schema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password, mobile } = req.body;
+  const isUserExists = await User.findOne({ email });
+
+  if (isUserExists) {
+    return res.status(400).json({ message: "user already exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      mobile,
+    });
+    res.status(200).json({ message: "user created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "user not found" });
+  }
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "password is incorrect" });
+  }
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  res.status(200).json({ token });
+});
+module.exports = router;
